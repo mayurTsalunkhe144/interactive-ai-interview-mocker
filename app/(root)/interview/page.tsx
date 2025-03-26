0d02037909605772127bcb8d31a3ee010c2335b1
Mayur <mayurtsalunkhe@gmail.com>
2025-03-26 19:41:22 +0530

    interview page added

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -1,7 +1,13 @@
+import Agent from "@/components/ui/Agent";
 import React from "react";
 
 const page = () => {
-  return <div>interview</div>;
+  return (
+    <div>
+      <h3>Interview Generation</h3>
+      <Agent userName="you" userId="user1" type="generate" />
+    </div>
+  );
 };
 
 export default page;
