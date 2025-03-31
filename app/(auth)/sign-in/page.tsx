a679472d9a1ab11f9deafbeaf0c9b2414bb5f604
Mayur <mayurtsalunkhe@gmail.com>
2025-03-31 23:40:58 +0530

    seo testing

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,10 +1,16 @@
-import AuthForm from '@/components/ui/AuthForm'
-import React from 'react'
-
-const page = () => {
-  return (
-    <AuthForm type="sign-in" />
-  )
+import AuthForm from "@/components/ui/AuthForm";
+import React from "react";
+export async function generateMetadata() {
+  return {
+    title: "AI Mock Interview Platform",
+    description: "Practice AI-powered mock interviews and ace your job search.",
+    other: {
+      "google-site-verification": "W5cIYmHjQWGx8gun7Ru9L4QGXEQD1EaQURHqDXVWrlA",
+    },
+  };
 }
+const page = () => {
+  return <AuthForm type="sign-in" />;
+};
 
-export default page
+export default page;
